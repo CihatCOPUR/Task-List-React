633a50b4c6f3e65937d74774f6e0b81b9b8185d6
Cihat <copurcihat@gmail.com>
2022-11-03 14:15:59 +0300

    button

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,17 +1,40 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
-
-
-
+import { useState ,useEffect} from "react";
+import axios from "axios";
 
 const Home = () => {
-  return (
-    <Container>
-        <AddTask/>
-        <TaskList/>
-    </Container>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export  default Home;
+  const [text, setText] = useState("Show Task ");
+
+  const toggle = () => {
+    setOpen(!open);
+    setText(open ? "Show tasks" : "Hide tasks");
+  };
+
+  const url="https://6363964037f2167d6f7d3e8c.mockapi.io/task";
+
+  useEffect(() => {
+   axios(url).then((res)=>console.log(res.data))
+  }, [])
+  
+
+  return (
+    <Container className="home">
+      <Button
+        onClick={() => {
+          toggle();
+        }}
+      >
+        {text}
+      </Button>
+      {open && <AddTask />}
+
+      <TaskList />
+    </Container>
+  );
+};
+
+export default Home;
